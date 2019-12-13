5fbd88d48a5502b8fdc24ce04274b4ac28ef22cb
backsapce <backsapce@hotmail.com>
2019-12-13 03:56:50 +0300

    Implemented account controller with authentication login method.

--- SecurityCourse/ClientApp/src/app/home/home.component.ts
+++ SecurityCourse/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Injectable, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { concatMap, switchMap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+    constructor(private http: HttpClient) { }
+
+    private isLoggedInParam = false;
+
+    public login(data: { email, password }): Observable<boolean> {
+        return this.http.post("/Account/Login", { login: data.email, password: data.password })
+            .pipe(map((x: any) => {
+                return this.isLoggedInParam = !!x.isAuthorized;
+            })
+        );
+    }
+
+    public logout(): void {
+      this.isLoggedInParam = false;
+    }
+
+    public isLoggedIn(): boolean {
+      return this.isLoggedInParam;
+    }
+}
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    loginForm: FormGroup;
+    isSubmitted = false;
+
+    constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder ) {  }
+
+    ngOnInit() {
+      this.loginForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      });
+    }
+
+    get formControls() { return this.loginForm.controls; }
+
+    login() {
+        console.log(this.loginForm.value);
+        this.isSubmitted = true;
+
+        if (this.loginForm.invalid) {
+          return;
+        }
+
+        this.authService.login(this.loginForm.value).subscribe(
+            x => {
+                if (x) {
+                  this.router.navigateByUrl('/admin');
+                } else {
+                  this.isSubmitted = false;
+                } 
+            },
+            err => this.isSubmitted = false
+        );
+    }
 }
---
+++ SecurityCourse/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Driver;
+
+namespace SecurityCourse.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMongoDatabase _mongoDatabase;
+        private readonly IMongoCollection<Account> _accounts;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+            _mongoDatabase = new MongoClient("mongodb://localhost").GetDatabase("SecuredDatabase");
+            _accounts = _mongoDatabase.GetCollection<Account>("Accounts");
+
+            if (!_accounts.AsQueryable().Any())
+            {
+                _accounts.InsertOne(new Account {Login = "root", Password = "hard_password"});
+            }
+        }
+
+        [HttpPost("[action]")]
+        public AuthResponse Login([FromBody] AuthRequest request)
+        {
+            var document = BsonDocument.Parse($@"{{
+                                                     Login : ""{request.Login}"" , Password: /{request.Password}/i
+                                                }}");
+
+            var accountFound = _accounts.Find(document).Any();
+
+            return new AuthResponse
+            {
+                IsAuthorized = accountFound
+            };
+        }
+    }
+
+    public class Account
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        public object Id { get; set; }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class AuthResponse
+    {
+        public bool IsAuthorized { get; set; }
+    }
+
+    public class AuthRequest
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
